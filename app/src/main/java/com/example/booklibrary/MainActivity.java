8b468e4396f260c2416c8965b1dd71c1feee949c
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-07-07 16:31:40 +0800

    Implemented: retrieved RecyclerView and implemented onclick functionality on add button

--- app/src/main/java/com/example/booklibrary/MainActivity.java
+++ app/src/main/java/com/example/booklibrary/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.booklibrary;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    FloatingActionButton add_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,16 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        recyclerView = findViewById(R.id.recyclerView);
+        add_button = findViewById(R.id.button_add);
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Activity_Add.class);
+                startActivity(intent);
+            }
         });
     }
 }
