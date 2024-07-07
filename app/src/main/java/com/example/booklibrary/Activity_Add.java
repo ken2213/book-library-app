302f6123bd62787a1e02a60dd8be6f94145651b2
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-07-07 21:59:42 +0800

    Implemented: adding to db functionality

--- app/src/main/java/com/example/booklibrary/Activity_Add.java
+++ app/src/main/java/com/example/booklibrary/Activity_Add.java
@@ -1,6 +1,9 @@
 package com.example.booklibrary;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Activity_Add extends AppCompatActivity {
+
+    EditText input_title, input_author, input_pages;
+    Button button_add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,26 @@ public class Activity_Add extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Retrieve inputs from layout by their id
+        input_title = findViewById(R.id.input_title);
+        input_author = findViewById(R.id.input_author);
+        input_pages = findViewById(R.id.input_pages);
+
+        // Retrieve button from layout by its id
+        button_add = findViewById(R.id.button_add_item);
+
+        button_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyDatabaseHelper myDB = new MyDatabaseHelper(Activity_Add.this);
+                myDB.addBook(
+                        input_title.getText().toString().trim(),
+                        input_author.getText().toString().trim(),
+                        Integer.valueOf(input_pages.getText().toString().trim())
+                );
+            }
+        });
+
     }
 }
